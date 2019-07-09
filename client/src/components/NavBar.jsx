fcc1442a436f1d1412e24664471525fc823577bc
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2019-07-09 10:47:16 +0300

    css for selectors

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,24 +6,26 @@ const NavBar = ({ artists, selectedArtist, onSelectArtist, types, selectedType, 
   return (
     <header className="app-header">
       <h1 className="app-header__title">Queen Collection</h1>
-      {artists &&
-        <Selector
-          items={artists}
-          selectedItem={selectedArtist}
-          onSelect={onSelectArtist}
-        />
-      }
-      {types &&
-        <Selector
-          items={types}
-          selectedItem={selectedType}
-          onSelect={onSelectType}
-          name="types"
-        />
-      }
-      {entries &&
-        <input className="entries-filter" type="text" placeholder="Filter entries" value={entryFilterText} onChange={onChangeEntryFilterText} />
-      }
+      <div className="app-header__selectors" >
+        {artists &&
+          <Selector
+            items={artists}
+            selectedItem={selectedArtist}
+            onSelect={onSelectArtist}
+          />
+        }
+        {types &&
+          <Selector
+            items={types}
+            selectedItem={selectedType}
+            onSelect={onSelectType}
+            name="types"
+          />
+        }
+        {entries &&
+          <input className="entries-filter" type="text" placeholder="Filter entries" value={entryFilterText} onChange={onChangeEntryFilterText} />
+        }
+      </div>
     </header>
   )
 }

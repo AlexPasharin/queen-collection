0959695db0f9c34202d08db11c0a6ea286229f5c
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2019-07-31 22:27:58 +0300

    redefine findIndex for arrays

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
+const _findIndex = Array.prototype.findIndex
+
+Array.prototype.findIndex = function (predicate) {
+  const index = _findIndex.call(this, predicate)
+
+  if (index < 0)
+    return null
+
+  return index
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

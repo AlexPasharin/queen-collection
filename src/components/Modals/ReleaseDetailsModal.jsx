a1051bd929c4d676b9145034c8511c0bf81e1aa4
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2019-10-10 18:25:38 +0300

    restore dev ReleaseDetailsModal

--- src/components/Modals/ReleaseDetailsModal.jsx
+++ src/components/Modals/ReleaseDetailsModal.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import ReleaseDetails from './ReleaseDetails'
+import AddReleaseForm from './AddReleaseForm'
 import Modal from './Modal'
 
 import '../../styles/Modal.css'
 
-const ReleaseDetailsModal = ({ release, onClose }) => {
+const ReleaseDetailsModal = ({ release, onClose, addRelease, updateRelease, initialMode = 'details' }) => {
+  const [mode, setMode] = useState(initialMode)
 
   const onModalClose = () => {
     let allowClosing = true
+
+    if (mode !== 'details') {
+      allowClosing = window.confirm("You are about to close modal. All form information will be lost.")
+    }
 
     if (allowClosing) {
       onClose()
@@ -17,7 +23,15 @@ const ReleaseDetailsModal = ({ release, onClose }) => {
 
   return (
     <Modal onClose={onModalClose}>
-      <ReleaseDetails releaseData={release} />
+      {mode === 'details' ?
+        <ReleaseDetails releaseData={release} onCopy={() => setMode('add')} onEdit={() => setMode('edit')} /> :
+        <AddReleaseForm
+          initialReleaseData={release}
+          addRelease={addRelease}
+          updateRelease={updateRelease}
+          mode={mode}
+        />
+      }
     </Modal>
   )
 }
